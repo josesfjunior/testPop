05091b81475d7b26d7395489dd0e4ae4c0a6e008
José Júnior <josejuninho8@gmail.com>
2023-06-29 11:09:24 -0300

    Initial Commit

---
+++ testPop/testPopApp.swift
@@ -0,0 +1,17 @@
+//
+//  testPopApp.swift
+//  testPop
+//
+//  Created by José Júnior on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct testPopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
